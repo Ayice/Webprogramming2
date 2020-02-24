f7d5066d26d5ee25bad9fee048747f78cc4fac52
Anders <anders_thy@rocketmail.com>
2020-02-24 16:34:52 +0100

    Adding signup form to home

--- webprogramming-react/src/App.js
+++ webprogramming-react/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import LoginForm from './components/login-form/login-form'
+import SignUpForm from './components/signUpForm/signUpForm'
 
 function App() {
 	return (
 		<div className='App'>
 			<Navbar />
 			<LoginForm />
+			<SignUpForm />
 		</div>
 	)
 }
