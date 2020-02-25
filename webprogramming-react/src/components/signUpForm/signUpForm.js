c2bd231cf599a2b3c8a17a97b984845ca2dded45
anders_thy@rocketmail.com <anders_thy@rocketmail.com>
2020-02-25 09:15:34 +0100

    merge 2

--- webprogramming-react/src/components/signUpForm/signUpForm.js
+++ webprogramming-react/src/components/signUpForm/signUpForm.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react'
-import firebase from 'firebase'
-import fire from '../../firebase'
-import './signUpStyles.css'
+import React, { Component } from 'react';
+import firebase from 'firebase';
+import fire from '../../firebase';
+import './signUpStyles.css';
 
 export default class SignUpForm extends Component {
-	constructor(props) {
-		super(props)
+    constructor(props) {
+        super(props);
 
-		this.initialState = {
+        this.initialState = {
 			name: '',
-			address: '',
-			email: '',
-			username: '',
-			password: ''
-		}
+            address: '',
+            email: '',
+            username: '',
+            password: ''
+        }
+        
+        this.state = this.initialState;
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-		this.state = this.initialState
-		this.handleSubmit = this.handleSubmit.bind(this)
-	}
-
-	handleChange = event => {
+    handleChange = event => {
 		const { name, value } = event.target
 		this.setState({
 			[name]: value
 		})
-	}
+    }
+    
+    handleSubmit = users => {
 
-	handleSubmit = users => {
 		// Tilføjer users til databasen
 		fire.collection('users').add({
 			name: users.name,
@@ -34,63 +35,60 @@ export default class SignUpForm extends Component {
 			email: users.email,
 			username: users.username,
 			password: users.password
-		})
-		firebase
-			.auth()
-			.createUserWithEmailAndPassword(users.email, users.password)
-			.catch(function(error) {
-				// Handle Errors here.
-				var errorCode = error.code
-				var errorMessage = error.message
-				// ...
-			})
-		console.log('test')
-		this.setState(this.initialState)
-	}
+        });
+        firebase.auth().createUserWithEmailAndPassword(users.email, users.password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+          });
+        console.log('test');
+        this.setState(this.initialState);
 
-	render() {
-		const { name, address, email, username, password } = this.state
-		return (
-			<div>
-				<form
-					onSubmit={e => {
-						e.preventDefault()
-						this.handleSubmit(this.state)
-					}}
-				>
-					<div>
-						<h1>Sign up, my dude</h1>
-					</div>
+  }
 
-					<fieldset>
-						<div>
-							{/* Value = name/time i state dette er for at vi kan tømme inputs senere */}
-							<input type='text' name='name' id='name' value={name} placeholder='Enter name here' onChange={this.handleChange} />
-						</div>
 
-						<div>
-							<input type='text' name='address' id='address' value={address} placeholder='Enter your address here' onChange={this.handleChange} />
-						</div>
+    render() {
+        const {name, address, email, username, password} = this.state
+        return (
+            <div>
+                
+                <form onSubmit={(e) =>  {e.preventDefault(); this.handleSubmit(this.state)}}>
+                    
+                    <div>
+                        <h1>Sign up, my dude</h1>
+                    </div>
+                    
+                    <fieldset>
 
-						<div>
-							<input type='text' name='email' id='email' value={email} placeholder='Enter a valid e-mail here' onChange={this.handleChange} />
-						</div>
+                        <div>
+                            {/* Value = name/time i state dette er for at vi kan tømme inputs senere */}
+                            <input type='text' name='name' id='name' value={name} placeholder='Enter name here' onChange={this.handleChange} />
+                        </div>
 
-						<div>
-							<input type='text' name='username' id='username' value={username} placeholder='Enter a valid username' onChange={this.handleChange} />
-						</div>
+                        <div>
+                            <input type='text' name='address' id='address' value={address} placeholder='Enter your address here' onChange={this.handleChange} />
+                        </div>
 
-						<div>
-							<input type='password' name='password' id='password' value={password} placeholder='Enter a valid password' onChange={this.handleChange} />
-						</div>
-						<div>
-							<button type='submit' value='Submit'>
-								Submit
-							</button>
-						</div>
-					</fieldset>
-				</form>
-			</div>
-		)
-	}
+                        <div>
+                            <input type='text' name='email' id='email' value={email} placeholder='Enter a valid e-mail here' onChange={this.handleChange} />
+                        </div>
+
+                        <div>
+                            <input type='text' name='username' id='username' value={username} placeholder='Enter a valid username' onChange={this.handleChange} />
+                        </div>
+                        
+                        <div>
+                            <input type='password' name='password' id='password' value={password} placeholder='Enter a valid password' onChange={this.handleChange} />
+                        </div>
+                        <div>
+                            <button type='submit' value='Submit'>
+                                Submit
+                            </button>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        )
+    }
 }
