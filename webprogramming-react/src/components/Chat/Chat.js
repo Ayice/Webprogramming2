5a585f50fb763cbcb0cc48e9aaf9aa2cf90f2e11
Anders <anders_thy@rocketmail.com>
2020-02-28 15:40:02 +0100

    Removed Placeholder Chats

--- webprogramming-react/src/components/Chat/Chat.js
+++ webprogramming-react/src/components/Chat/Chat.js
@@ -1,4 +1,4 @@
-import React , {Component} from 'react'
+import React, { Component } from 'react'
 import fire from '../../firebase'
 import firebase from 'firebase'
 import 'firebase/auth'
@@ -6,102 +6,117 @@ import './Chat.css'
 import { useParams } from 'react-router-dom'
 
 class Chat extends Component {
-    constructor(props) {
+	constructor(props) {
 		super(props)
 
 		this.state = {
-            currentUser: {},
-            currentChatroom: { id: props.match.params.id },
-            messages: [],
-            text: ''
-        }
+			currentUser: {},
+			currentChatroom: { id: props.match.params.id },
+			messages: [],
+			text: ''
+		}
 
-        this.sendMessage = this.sendMessage.bind(this)
-    }
-    
-    componentDidMount(){
-        console.log(this.state)
-        fire.collection('chatrooms').doc(this.state.currentChatroom.id).get()
-        .then(doc => {
-            console.log(doc.data());
-            this.setState({
-                currentChatroom: {id: doc.id, ...doc.data() }
-            })
-        })
-        .then(() => {fire.collection('messages').doc(this.state.currentChatroom.id).collection('messages').onSnapshot(doc => {
-            let fetchedMsgs = []
-            fetchedMsgs = []
-            doc.forEach(msg => {
-                fetchedMsgs.push(msg.data())
-                this.setState({
-                    messages: fetchedMsgs
-                })
-            })
-        })})
-    }
-    
-    handleChange = event => {
+		this.sendMessage = this.sendMessage.bind(this)
+	}
+
+	componentDidMount() {
+		console.log(this.state)
+		fire
+			.collection('chatrooms')
+			.doc(this.state.currentChatroom.id)
+			.get()
+			.then(doc => {
+				console.log(doc.data())
+				this.setState({
+					currentChatroom: { id: doc.id, ...doc.data() }
+				})
+			})
+			.then(() => {
+				fire
+					.collection('messages')
+					.doc(this.state.currentChatroom.id)
+					.collection('messages')
+					.onSnapshot(doc => {
+						let fetchedMsgs = []
+						fetchedMsgs = []
+						doc.forEach(msg => {
+							fetchedMsgs.push(msg.data())
+							this.setState({
+								messages: fetchedMsgs
+							})
+						})
+					})
+			})
+	}
+
+	handleChange = event => {
 		const { name, value } = event.target
 		this.setState({
 			[name]: value
 		})
 	}
 
-    sendMessage = event => {
-        var today = new Date();
-        var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        var timestamp = date +' '+ time;
+	sendMessage = event => {
+		var today = new Date()
+		var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear()
+		var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+		var timestamp = date + ' ' + time
 
-        fire.collection('messages').doc(this.state.currentChatroom.id).collection('messages').add({
-            text: this.state.text,
-            sender: this.props.currentUser.username,
-            timestamp: timestamp
-        })
-        .then(function() {
-            console.log("Message successfully sent!");
-        })
-        .catch(function(error) {
-            console.error("Error sending message: ", error);
-        });
-        console.log(this.state.text, this.state.currentUser.username, timestamp );
-        this.setState({
-            text: ''
-        })
+		fire
+			.collection('messages')
+			.doc(this.state.currentChatroom.id)
+			.collection('messages')
+			.add({
+				text: this.state.text,
+				sender: this.props.currentUser.username,
+				timestamp: timestamp
+			})
+			.then(function() {
+				console.log('Message successfully sent!')
+			})
+			.catch(function(error) {
+				console.error('Error sending message: ', error)
+			})
+		console.log(this.state.text, this.state.currentUser.username, timestamp)
+		this.setState({
+			text: ''
+		})
 	}
 
-    render() {
-        const { messages, text } = this.state
-        return (
-            <div id="chat-section">
-                <div className="chat-header"><h2>{this.state.currentChatroom.name}</h2></div>
-                <div className="message-container">
-                {messages
-                // should sort the messages so last sent shows up first (from the bottom) use .sort()
-                // fix that message-container shows bottom first, so you don't have to scroll down to see latest message
-                .map((element, index) => {
+	render() {
+		const { messages, text } = this.state
+		return (
+			<div id='chat-section'>
+				<div className='chat-header'>
+					<h2>{this.state.currentChatroom.name}</h2>
+				</div>
+				<div className='message-container'>
+					{messages
+						// should sort the messages so last sent shows up first (from the bottom) use .sort()
+						// fix that message-container shows bottom first, so you don't have to scroll down to see latest message
+						.map((element, index) => {
 							return (
-                            <div className={'message ' + (this.props.currentUser.username === element.sender ?  'sent' : 'received')} key={index}>{element.sender}: {element.text}
+								<div className={'message ' + (this.props.currentUser.username === element.sender ? 'sent' : 'received')} key={index}>
+									{element.sender}: {element.text}
 								</div>
 							)
 						})}
-                    <div className="message sent">Sent message example</div>
-                    <div className="message received">Received message example</div>
+				</div>
 
-                </div>
-                <form onSubmit={e => {
+				<form
+					onSubmit={e => {
 						e.preventDefault()
 						this.sendMessage(this.state)
 					}}
-                    >
-                    <textarea name="text" value={text} placeholder="Write a message..." rows="5" onChange={this.handleChange}></textarea>
-                    <button type='submit' value='Send'>
+				>
+					<textarea name='text' value={text} placeholder='Write a message...' rows='5' onChange={this.handleChange}></textarea>
+					<button type='submit' value='Send'>
 						Send
 					</button>
-                </form>
-            </div>
-        )
-    }
+				</form>
+			</div>
+		)
+	}
 }
 
 export default Chat
