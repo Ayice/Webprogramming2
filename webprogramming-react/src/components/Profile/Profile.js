b6bf30916000bd5f765d5f487d4aed8bccdc27e4
Sebastian Petersen <sebpet1989@gmail.com>
2020-03-02 22:25:06 +0100

    Added profile form skeleton. Needs functionality

--- webprogramming-react/src/components/Profile/Profile.js
+++ webprogramming-react/src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Profile.css';
 
 class Profile extends Component {
 	constructor(props) {
@@ -16,11 +17,47 @@ class Profile extends Component {
 
 	render() {
 		return (
-			<div>
-				<h1>{this.props.currentUser.password}</h1>
-				<form>
-					<input value={this.props.currentUser.username} />
+			<div className="profileContainer">
+				<h1>{this.props.currentUser.name}</h1>
+				<form className="profileForm">
+					
+					<div className="profileFormfield">
+						<label>Name</label>
+						<input value={this.props.currentUser.name} />
+					</div>
+
+					<div className="profileFormfield">
+						<label>Address</label>
+						<input value={this.props.currentUser.address} />
+					</div>
+
+					<div className="profileFormfield">
+						<label>E-mail</label>
+						<input value={this.props.currentUser.email} />
+					</div>
+
+					<div className="profileFormfield">
+						<label>Username</label>
+						<input value={this.props.currentUser.username} />
+					</div>
+
+					<div className="profileFormfield">
+						<label>Current Password</label>
+						<input value={this.props.currentUser.password} type="password"/>
+					</div>
+
+					<div className="profileFormfield">
+						<label>New Password</label>
+						<input value={this.props.currentUser.newpassword} placeholder="Enter new password" type="password" />
+					</div>
+
+					<div className="buttonContainer">
+						<button className="saveButton" type="submit">Save Changes</button>
+						<button className="manageContactsButton" type="button">Manage Contacts</button>
+						<button className="deleteAccountButton" type="submit">Delete Account</button>
+					</div>
 				</form>
+				
 			</div>
 		)
 	}
