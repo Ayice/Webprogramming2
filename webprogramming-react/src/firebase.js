b3afeb73d5b0f4d0a64e626e80760a28ab7c3acf
anders_thy@rocketmail.com <anders_thy@rocketmail.com>
2020-03-03 11:13:42 +0100

    test

--- webprogramming-react/src/firebase.js
+++ webprogramming-react/src/firebase.js
@@ -1,4 +1,4 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
 
 var firebaseConfig = {
 	apiKey: 'AIzaSyBkvdT8L4y1L3kTuWxh4bXBvewO3BOL2tk',
@@ -11,9 +11,8 @@ var firebaseConfig = {
 }
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
-const fire = firebase.firestore()
-const storage = firebase.storage()
+const fire = firebase.firestore();
 
-export default { fire, storage }
+export default fire;
