a897480a98fd26a5f01bf3ce4d2e2b22501505ac
Sebastian Petersen <sebpet1989@gmail.com>
2020-03-04 18:10:00 +0100

    Fixed redirect to Contacts, tried editing

--- webprogramming-react/src/App.js
+++ webprogramming-react/src/App.js
@@ -164,6 +164,24 @@ class App extends Component {
 			})
 	}
 
+	editUser = () => {
+		const user = firebase.auth().currentUser
+
+		fire
+			.collection('users')
+			.doc(user)
+			.set({
+				[user]: true
+				},
+				{ merge: true }
+			)
+			.then(() => {
+				alert('User information updated')
+			})
+			.catch((error) => {
+				console.log('you done goofed')
+				});
+	}
 
 	removeUser = () => {
 		const user = firebase.auth().currentUser
